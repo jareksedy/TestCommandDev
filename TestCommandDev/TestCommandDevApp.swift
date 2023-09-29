cb8c77f0ae7cb7bff78956249490d108f6206674
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2023-09-29 20:29:55 +0600

    Initial Commit

---
+++ TestCommandDev/TestCommandDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCommandDevApp.swift
+//  TestCommandDev
+//
+//  Created by Ярослав on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestCommandDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
