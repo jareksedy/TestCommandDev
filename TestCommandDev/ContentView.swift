aac1ac7906d11f77521cb9b944ce82c455fcf0b9
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-09-29 20:42:36 +0600

    text 2

--- TestCommandDev/ContentView.swift
+++ TestCommandDev/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.mint
+            Color.blue
             VStack {
                 Image(systemName: "globe")
                     .imageScale(.large)
